88f0ad2eb5b8dfcf6b546bc3779a7dee09fb3b29
Matt-Camerato <matt.camerato@gmail.com>
2022-09-09 21:14:54 -0400

    Revisions
    Separated some scripts to make the mechanic more modular, then re-exported the Unity package.

--- Assets/Magnet.cs
+++ Assets/Magnet.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Magnet : MonoBehaviour
 {
-    [HideInInspector] public Transform playerTransform;
-
     [Header("Magnet Settings")]
     [SerializeField] private float _attractSpeed;
     [SerializeField] private float _collisionDelay;
@@ -14,16 +12,6 @@ public class Magnet : MonoBehaviour
     private Rigidbody2D _rb;
 
     private void Awake() => StartCoroutine(CollisionDelay());
-    private void Start() => _rb = GetComponent<Rigidbody2D>();
-
-    private void FixedUpdate()
-    {
-        if (!Input.GetKey(KeyCode.Space)) return; //don't do anything unless space is pressed
-
-        //add force towards the player's position
-        Vector2 moveDir = playerTransform.position - transform.position;
-        _rb.AddForce(moveDir.normalized * _attractSpeed);
-    }
 
     //enables the magnet's box collider after it first leaves the player's body (so it isn't destroyed instantly)
     private IEnumerator CollisionDelay()
@@ -32,6 +20,18 @@ public class Magnet : MonoBehaviour
         GetComponent<BoxCollider2D>().enabled = true;
     }
 
+    private void Start() => _rb = GetComponent<Rigidbody2D>();
+    private void OnEnable() => PlayerController.Attract += Attract;
+    private void OnDisable() => PlayerController.Attract -= Attract;
+
+    private void Attract(Transform player)
+    {
+        //add force towards the player's position
+        Vector2 moveDir = player.position - transform.position;
+        _rb.AddForce(moveDir.normalized * _attractSpeed);
+    }
+
+    //destroy magnet when it collides with the player
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) Destroy(gameObject);
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -6,18 +7,25 @@ public class PlayerController : MonoBehaviour
     [Header("Player Settings")]
     [SerializeField] private float _moveSpeed;
 
-    [Header("Magnet Settings")]
-    [SerializeField] private float _magnetThrowPower;
-    [SerializeField] private GameObject _magnetPrefab;
+    public static event Action Throw;
+    public static event Action<Transform> Attract;
 
     private Rigidbody2D _rb;
 
     private void Start() => _rb = GetComponent<Rigidbody2D>();
-    private void FixedUpdate() => HandleMovement();
-    private void Update() => HandleThrowing();
 
-    //handles all player movement
-    private void HandleMovement()
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Throw.Invoke(); //invoke throw event if LMB is pressed
+    }
+
+    private void FixedUpdate()
+    {
+        HandlePlayerMovement();
+        if (Input.GetKey(KeyCode.Space)) Attract.Invoke(transform); //invoke attract event if pressing space
+    }
+
+    private void HandlePlayerMovement()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
@@ -25,19 +33,5 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDir = new Vector3(x, y, 0f);
         _rb.AddForce(moveDir * _moveSpeed);
-    }
-
-    //handles throwing magnets when LMB is pressed
-    private void HandleThrowing()
-    {
-        if (!Input.GetMouseButtonDown(0)) return; //don't continue unless player presses LMB
-
-        //create the magnet and give it reference to the player's transform
-        GameObject magnet = Instantiate(_magnetPrefab, transform.position, Quaternion.identity);
-        magnet.GetComponent<Magnet>().playerTransform = transform;
-
-        //apply an impulse force to the magent in the direction of the player's mouse
-        Vector2 forceDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        magnet.GetComponent<Rigidbody2D>()?.AddForce(forceDir.normalized * _magnetThrowPower, ForceMode2D.Impulse);
     }
 }
